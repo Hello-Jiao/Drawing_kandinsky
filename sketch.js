55bccc779664298001553a3ad35ec7045b9617a1
Jiao Wang <jiao228228@gmail.com>
2020-05-04 21:18:12 +0200

    js_drawing

---
+++ sketch.js
@@ -0,0 +1,84 @@
+function setup()
+{
+	//create your canvas here
+    createCanvas(500,500);
+}
+
+function draw()
+{
+	//do your drawing here
+    fill(203,168,116,150);
+    noStroke()
+    triangle(584,-83,-90,440,44,600);
+    //bigBG blue triangle
+    fill(123,164,158,150);
+    noStroke()
+    triangle(-77,-78,565,536,708,383);
+    //cricle
+    strokeWeight(20);
+    stroke(0,0,0,);
+    noFill();
+    ellipse(250,250,433,433);
+    //leftyellow.ellipse
+    noStroke();
+    fill(173,178,35,180);
+    ellipse(198,323,145,145);
+    //pinklefttop
+    fill(226,168,171,180);
+    ellipse(186,201,122,122);
+    //rightmid
+    fill(52,101,82,180);
+    ellipse(349,250,125,125);
+    //bigbrown
+    fill(94,89,58,180);
+    ellipse(251,302,151,151);
+    //redmid
+    fill(172,44,43,180);
+    ellipse(267,198,88,88);
+    //rightdownyellow
+    fill(231,199,114,180);
+    stroke(0,0,0);
+    strokeWeight(3);
+    ellipse(314,333,88,88);
+    //leftpurple
+    fill(106,68,83,180);
+    noStroke();
+    ellipse(338,172,53,53);
+    //midorange
+    fill(191,94,60,180);
+    strokeWeight(5);
+    stroke(0,0,0);
+    ellipse(234,280,59,59);
+    //leftdownred
+    fill(172,44,43,180);
+    noStroke();
+    ellipse(143,348,36,36);
+    //leftredup
+    fill(214,33,33,180);
+    strokeWeight(5);
+    stroke(0,0,0);
+    ellipse(113,239,22,22);
+    //miduppink
+    strokeWeight(3);
+    fill(214,40,109,180);
+    ellipse(250,109,40,40);
+    //little right
+    fill(0,0,0);
+    ellipse(337,366,22,22);
+    noStroke();
+    fill(216,221,91,190);
+    ellipse(279,395,20,20);
+    //linns
+    stroke(0,0,0,);
+    line(311,100,94,302);
+    line(348,100,186,405);
+    line(358,155,369,385);
+    line(198,100,395,275);
+    line(79,261,388,340);
+    line(143,130,421,198);
+    line(287,100,308,137);
+    line(280,110,301,145);
+    line(153,377,250,366);
+    line(157,385,255,375);
+    
+}
